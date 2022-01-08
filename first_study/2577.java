385f43965ea7a15a8092085f86171838b034d3f8
JAE HONG LEE <69387517+lijahong@users.noreply.github.com>
2022-01-08 22:43:11 +0900

    Create 2577.java

---
+++ first_study/2577.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FastReader fr = new FastReader(); 
+        int[] cnt = new int[10];
+        int a = fr.nextInt();
+        int b = fr.nextInt();
+        int c = fr.nextInt();
+        int d = a*b*c;
+        String tmp = Integer.toString(d);
+        for(int i =0; i< tmp.length(); i++){
+          int a1 = Integer.parseInt(tmp.charAt(i)+"");
+          cnt[a1]+= 1;
+        }
+        for(int i = 0; i<10;i++){
+          System.out.println(cnt[i]);
+        }
+       
+        
+       
+        }
+        
+    
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
