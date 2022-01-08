d8c6bfe2c366f2bbff94370fe3d772dfcdddce84
JAE HONG LEE <69387517+lijahong@users.noreply.github.com>
2022-01-08 22:47:12 +0900

    자바 입력시 가장 빠른 기법

---
+++ input.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+//입력 사용시 스캐너보다 훨씬 빠르게 실행되는 input 기법
+public class Main {
+        public static void main(String[] args) {
+           FastReader fr = new FastReader();
+         
+           
+           
+           
+     }
+       public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
